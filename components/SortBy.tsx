a46ec67f0b42706b751caee539a9cb335ed3762d
DijanaSelv <d.selvijovska@gmail.com>
2026-06-06 13:36:44 +0200

    sort by dropdown works by clicking and hovering as well so that we can open it on mobile as well. Added use effect handler so that clicking outsude closes the dropdown.

--- components/SortBy.tsx
+++ components/SortBy.tsx
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from "react";
+
 const SortBy = ({
   selectedSortTerm,
   sortDescendingValue,
@@ -11,11 +13,39 @@ const SortBy = ({
   setSortTerm: (value: string) => void;
   setSortDescending: (value: boolean) => void;
 }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("pointerdown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("pointerdown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="flex items-center justify-end divide-x divide-slate-500 border border-slate-500 rounded-sm p-1">
-      <p className="px-4 max-sm:hidden">sort by:</p>
-      <div className=" px-4 cursor-pointer transition-class outline-none appearance-none relative min-w-32 group">
+      <p className="px-4 max-lg:hidden">sort by:</p>
+      <div
+        className=" px-4 cursor-pointer transition-class outline-none appearance-none relative min-w-32 group"
+        onClick={() => setIsOpen((prev) => !prev)}
+        ref={dropdownRef}
+      >
         <p className="">
+          <span className="text-xs lg:hidden pr-3">
+            {" "}
+            <i className="fa-solid fa-sort  "></i>{" "}
+          </span>
           {selectedSortTerm == "last_played"
             ? "date"
             : selectedSortTerm == "session_count"
@@ -25,7 +55,17 @@ const SortBy = ({
                 : selectedSortTerm}
         </p>
 
-        <div className="absolute w-full z-20 left-0 pt-2 opacity-0 group-hover:opacity-100 -translate-y-2 group-hover:translate-y-0 pointer-events-none group-hover:pointer-events-auto transition-class">
+        <div
+          className={`absolute w-full z-20 left-0 pt-2 transition-class
+            ${
+              isOpen
+                ? "opacity-100 translate-y-0 pointer-events-auto"
+                : "opacity-0 -translate-y-2 pointer-events-none"
+            }
+            group-hover:opacity-100
+            group-hover:translate-y-0
+            group-hover:pointer-events-auto`}
+        >
           <div className="flex flex-col bg-foreground text-background *:py-1 *:px-2  *:hover:bg-background/10 ">
             {sortTerms.map((term, i) => (
               <p
